005585d849047e8856a6adecf270de6210a49d88
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-19 13:27:29 +0200

    event click

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,10 @@
 /* 
 Descrizione:
 Rifare l'esercizio dello slider come fatto assieme in classe.
+1. salvare il valore dell'index della singola immagine
+2. variare il valore dell'index per consentire all'immagine di cambiare
+3. creare un evento click sulle freccie collegando il cambio di valore dell'index
+
 //--------------------------------------------------------------------------
 Bonus:
 Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
@@ -9,14 +13,26 @@ Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticament
 var app = new Vue({
     el: '#root',
     data: {
-        
+
         Images: [
             'https://www.coopalleanza3-0.it/dam/relation/images/placeholder/placeholder-content1-500x300.png',
             'https://ftl-media.imgix.net/media/truck/flat-bed/114sd-blue-500x300.jpg',
             'https://www.fromoldbooks.org/Bell-BritishLocomotivesIllustrated/24-Re-constructed-Atlantic-type-Locomotive-q75-500x300.jpg'
         ],
-
+        // index della singola immagine salvato in una variabile
         ImageIndex: 0
-    }
+    },
+
+    methods: {
+        next: function() {
+
+            this.ImageIndex++
+        },
+
+        back: function() {
+
+            this.ImageIndex--
+        }
+    },
 });
 
